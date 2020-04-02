99503fdf55f3c7a3f1e4d3aff93681c3f71600c9
Lucien Gaitskell <lucieng@gaitskell.com>
2020-04-02 09:09:28 -0400

    Replace card_crop with canny filter

--- ocr/card_crop.py
+++ ocr/card_crop.py
@@ -5,6 +5,7 @@ based on work for a playing card detector: https://github.com/EdjeElectronics/Op
 """
 
 import cv2
+import imutils
 import numpy as np
 
 # Setup variables:
@@ -18,111 +19,57 @@ CARD_MIN_AREA = 25000
 im = cv2.imread(IMG_INP)  # Load image
 res_img = im.copy()  # Copy image for later result
 
-# Basic initial conversions
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)  # convert to grayscale
-blur = cv2.GaussianBlur(gray, (5, 5), 0)  # apply blur
-
-# The best threshold level depends on the ambient lighting conditions.
-# For bright lighting, a high threshold must be used to isolate the cards
-# from the background. For dim lighting, a low threshold must be used.
-# To make the card detector independent of lighting conditions, the
-# following adaptive threshold method is used.
-#
-# A background pixel in the center top of the image is sampled to determine
-# its intensity. The adaptive threshold is set at 50 (THRESH_ADDER) higher
-# than that. This allows the threshold to adapt to the lighting conditions.
-img_w, img_h = np.shape(im)[:2]
-bkg_level = gray[int(img_h / 100)][int(img_w / 2)]
-thresh_level = bkg_level + BKG_THRESH
-
-# Apply threshold to blurred image:
-retval, thresh = cv2.threshold(blur, thresh_level, 255, cv2.THRESH_BINARY)
-
-# Select contours on image
-cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 15, 225)
 
 # Display filtered image
-cv2.imshow("ID Card with filters", thresh)
+cv2.imshow("ID Card with filters", edged)
 
-# Display original image with contours:
-#cv2.drawContours(im, cnts, -1, (255,0,0), 3)
-
-if len(cnts) == 0:  # If there are no contours, do nothing
-    print("NONE")
-else:  # otherwise continue:
-
-    # Sort contour indexes by the area of the contour represented
-    index_sort = sorted(range(len(cnts)), key=lambda i: cv2.contourArea(cnts[i]), reverse=True)
-
-    # initialize empty sorted contour and hierarchy lists
-    cnts_sort = []
-    hier_sort = []
-
-    # Fill empty lists with sorted contour and sorted hierarchy. Now,
-    # the indices of the contour list still correspond with those of
-    # the hierarchy list. The hierarchy array can be used to check if
-    # the contours have parents or not.
-    for i in index_sort:
-        cnts_sort.append(cnts[i])
-        hier_sort.append(hier[0][i])
-
-    # Determine which of the contours are cards by applying the
-    # following criteria: 1) bigger area than the minimum card size, 2) have no parents,
-    # and 3) have four corners
-
-    # Resulting card data
-    card_approx = None
-    card_idx = None
-    card_contour = None
-
-    for i in range(len(cnts_sort)):
-
-        # Calculate area, perimeter, and approximate polygon for contour
-        size = cv2.contourArea(cnts_sort[i])
-        peri = cv2.arcLength(cnts_sort[i], True)
-        approx = cv2.approxPolyDP(cnts_sort[i], 0.05 * peri, True)
-
-        # Clarify contour filters from above
-        if ((size > CARD_MIN_AREA)
-                and (hier_sort[i][3] == -1) and (len(approx) == 4)):
-            card_approx = approx
-            card_idx = i
+# find contours in the edge map, then initialize
+# the contour that corresponds to the document
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cardCnt = None
+# ensure that at least one contour was found
+if len(cnts) > 0:
+    # sort the contours according to their size in
+    # descending order
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    # loop over the sorted contours
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        print(cv2.contourArea(c))
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # if our approximated contour has four points,
+        # then we can assume we have found the paper
+        if len(approx) == 4:
+            cardCnt = np.squeeze(approx, axis=1)
             break
 
-    if card_idx is not None:
-        # Draw the selected contour:
-        cv2.drawContours(im, cnts_sort, card_idx, (255, 0, 0), 3)
+cv2.polylines(im, [cardCnt], True, (0, 255, 255), 10)
 
-        # Select corner points and remove extra dimensions
-        corner_pts = np.float32(card_approx)
-        corner_pts = np.squeeze(corner_pts, axis=1)
 
-        def card_point(num):
-            return int(corner_pts[num][0]), int(corner_pts[num][1])
+def card_point(num):
+    return int(cardCnt[num][0]), int(cardCnt[num][1])
 
-        # Draw box around card, from corner points
-        for i in [(0,1), (1,2), (2,3), (3,0)]:
-            cv2.line(im, card_point(i[0]), card_point(i[1]), (255, 0, 0), 3)
 
-        # Show non-rotated bounding rectangle
-        x,y,w,h = cv2.boundingRect(cnts_sort[card_idx])
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# Draw box around card, from corner points
+for i in [(0,1), (1,2), (2,3), (3,0)]:
+    cv2.line(im, card_point(i[0]), card_point(i[1]), (255, 0, 0), 3)
 
-        # Perspective warp ID using corner points
-        # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
-        M = 600
-        RAT = 1.5
-        pts2 = np.float32([[M, 0], [0, 0], [0, M*RAT], [M, M*RAT]])
-        trans = cv2.getPerspectiveTransform(corner_pts, pts2)
-        dst = cv2.warpPerspective(res_img, trans, (M, int(M * RAT)))
-        cv2.imshow("Warped ID Card from contours", dst)
-        cv2.imwrite(IMG_OTP, dst)
+# Perspective warp ID using corner points
+# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
+M = 600
+RAT = 1.5
+pts2 = np.float32([[0, 0], [0, M*RAT], [M, M*RAT], [M, 0]])
+trans = cv2.getPerspectiveTransform(np.float32(cardCnt), pts2)
+dst = cv2.warpPerspective(res_img, trans, (M, int(M * RAT)))
+cv2.imshow("Warped ID Card from contours", dst)
+cv2.imwrite(IMG_OTP, dst)
 
 cv2.imshow("ID Card with contours", im)
-
-
-# TODO: Select contour of card
-# TODO: Select corners
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
