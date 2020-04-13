bfec3358d993918839f19e4a5ac7d21f791c1ef4
Lucien Gaitskell <lucieng@gaitskell.com>
2020-04-12 20:24:55 -0400

    WORKING: new HoughLines and intersection

--- ocr/card_crop.py
+++ ocr/card_crop.py
@@ -7,6 +7,7 @@ based on work for a playing card detector: https://github.com/EdjeElectronics/Op
 import cv2
 import imutils
 import numpy as np
+from scipy.spatial import cKDTree
 
 # Setup variables:
 IMG_NAME = 'id_front.jpeg'
@@ -78,20 +79,124 @@ cv2.imshow("ID Card with filters and only one contour", only_cnt)
 minLineLength = 500
 maxLineGap = 400
 
-lines = cv2.HoughLinesP(
+lines = cv2.HoughLines(
     only_cnt,
-    rho=20,
+    rho=1,
     theta=1*np.pi/180,
-    threshold=800,  # TODO: THIS IS making the operation very slow -- and needs a lot of refining ... find another way
-    minLineLength=minLineLength,
-    maxLineGap=maxLineGap)
+    threshold=200)
 print(len(lines))
-for i in range(len(lines)):
-    l = lines[i]
-    for x1,y1,x2,y2 in l:
-        color = [0,] * 3
-        color[i%3] = 255
-        cv2.line(im,(x1,y1),(x2,y2),color,10)
+for l in lines:
+    rho, theta = l[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(im,(x1,y1),(x2,y2),(0,0,255),2)
+
+# Find intersections from HoughLines
+# TODO: SLIM DOWN CODE, ADD COMMENTS
+# https://stackoverflow.com/questions/46565975/find-intersection-point-of-two-lines-drawn-using-houghlines-opencv
+from collections import defaultdict
+def segment_by_angle_kmeans(lines, k=2, **kwargs):
+    """Groups lines based on angle with k-means.
+
+    Uses k-means on the coordinates of the angle on the unit circle
+    to segment `k` angles inside `lines`.
+    """
+
+    # Define criteria = (type, max_iter, epsilon)
+    default_criteria_type = cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER
+    criteria = kwargs.get('criteria', (default_criteria_type, 10, 1.0))
+    flags = kwargs.get('flags', cv2.KMEANS_RANDOM_CENTERS)
+    attempts = kwargs.get('attempts', 10)
+
+    # returns angles in [0, pi] in radians
+    angles = np.array([line[0][1] for line in lines])
+    # multiply the angles by two and find coordinates of that angle
+    pts = np.array([[np.cos(2*angle), np.sin(2*angle)]
+                    for angle in angles], dtype=np.float32)
+
+    # run kmeans on the coords
+    labels, centers = cv2.kmeans(pts, k, None, criteria, attempts, flags)[1:]
+    labels = labels.reshape(-1)  # transpose to row vec
+
+    # segment lines based on their kmeans label
+    segmented = defaultdict(list)
+    for i, line in zip(range(len(lines)), lines):
+        segmented[labels[i]].append(line)
+    segmented = list(segmented.values())
+    return segmented
+
+
+segmented = segment_by_angle_kmeans(lines)
+
+def intersection(line1, line2):
+    """Finds the intersection of two lines given in Hesse normal form.
+
+    Returns closest integer pixel locations.
+    See https://stackoverflow.com/a/383527/5087436
+    """
+    rho1, theta1 = line1[0]
+    rho2, theta2 = line2[0]
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    x0, y0 = int(np.round(x0)), int(np.round(y0))
+    return [[x0, y0]]
+
+
+def segmented_intersections(lines):
+    """Finds the intersections between groups of lines."""
+
+    intersections = []
+    for i, group in enumerate(lines[:-1]):
+        for next_group in lines[i+1:]:
+            for line1 in group:
+                for line2 in next_group:
+                    intersections.append(intersection(line1, line2))
+
+    return intersections
+
+intersections = segmented_intersections(segmented)
+
+# Remove extra [] around elements (e.g. [[[a,b]], [c,d]]] -> [[a,b], [c,d]]
+intersections = np.squeeze(intersections, axis=1)
+
+# Finding close points:
+tree = cKDTree(intersections)
+rows_to_fuse = tree.query_pairs(r=30)  # Get indexes of close points
+print(rows_to_fuse)
+
+
+# TODO: THIS DOES NOT SEEM TO WORK CORRECTLY
+# https://stackoverflow.com/questions/36985185/fast-fuse-of-close-points-in-a-numpy-2d-vectorized
+idxs_to_remove = []
+for row in rows_to_fuse:
+    els = []
+    for idx in row:
+        els.append(intersections[idx])
+    s_idx = row.index(min(row))
+    for excess in row:
+        if not excess == row[s_idx]:
+            idxs_to_remove.append(excess)
+    m = np.mean(els, axis=1)
+    intersections[s_idx] = m
+
+intersections = np.delete(intersections, idxs_to_remove, axis=0)
+
+for item in intersections:
+    cv2.drawMarker(im, (item[0], item[1]),(0,0,255), markerType=cv2.MARKER_TILTED_CROSS,
+                   markerSize=40, thickness=2, line_type=cv2.LINE_AA)
+
+cv2.imshow("ID Card with contours", im)
 
 
 def card_point(num):
@@ -107,8 +212,6 @@ trans, status = cv2.findHomography(np.float32(cardCnt), pts2)
 dst = cv2.warpPerspective(res_img, trans, (M, int(M * RAT)))
 cv2.imshow("Warped ID Card from contours", dst)
 cv2.imwrite(IMG_OTP, dst)
-
-cv2.imshow("ID Card with contours", im)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
