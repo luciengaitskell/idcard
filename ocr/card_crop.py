4f13876a52662e059a28545512cb6e5015e4c321
Lucien Gaitskell <lucieng@gaitskell.com>
2020-04-16 20:42:14 -0400

    Extend display lines in HoughLines display

--- ocr/card_crop.py
+++ ocr/card_crop.py
@@ -91,10 +91,10 @@ for l in lines:
     b = np.sin(theta)
     x0 = a*rho
     y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+    x1 = int(x0 + 2000*(-b))
+    y1 = int(y0 + 2000*(a))
+    x2 = int(x0 - 2000*(-b))
+    y2 = int(y0 - 2000*(a))
 
     cv2.line(im,(x1,y1),(x2,y2),(0,0,255),2)
 
